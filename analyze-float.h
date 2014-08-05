9e53ad80235b1411c0d5785e0e164303aa5f1063
Rob Kennedy <kennedyri@gmail.com>
2014-08-05 06:35:02 -0700

    Change indentation style

--- analyze-float.h
+++ analyze-float.h
@@ -14,13 +14,13 @@ enum float_type { unknown, normal, zero, denormal, indefinite, infinity, quiet_n
 
 struct BigEndianExtended
 {
-	uint16_t exponent;
-	uint64_t mantissa;
+    uint16_t exponent;
+    uint64_t mantissa;
 };
 struct LittleEndianExtended
 {
-	uint64_t mantissa;
-	uint16_t exponent;
+    uint64_t mantissa;
+    uint16_t exponent;
 };
 
 typedef boost::mpl::if_c<BOOST_BYTE_ORDER == 1234, LittleEndianExtended, BigEndianExtended>::type Extended;
@@ -29,49 +29,49 @@ BOOST_STATIC_ASSERT(sizeof(Extended) == sizeof(long double));
 template <typename Float>
 struct float_traits
 {
-	static unsigned const mantissa_bits;
-	static unsigned const exponent_bits;
-	static signed const exponent_bias;
-	static bool const implied_one;
-	// typedef <unspecified> record_type;
+    static unsigned const mantissa_bits;
+    static unsigned const exponent_bits;
+    static signed const exponent_bias;
+    static bool const implied_one;
+    // typedef <unspecified> record_type;
 };
 
 template <>
 struct float_traits<long double>
 {
-	static unsigned const mantissa_bits = 64;
-	static unsigned const exponent_bits = 15;
-	static signed const exponent_bias = 16383;
-	static bool const implied_one = false;
-	typedef Extended record_type;
+    static unsigned const mantissa_bits = 64;
+    static unsigned const exponent_bits = 15;
+    static signed const exponent_bias = 16383;
+    static bool const implied_one = false;
+    typedef Extended record_type;
 };
 
 template <>
 struct float_traits<double>
 {
-	static unsigned const mantissa_bits = 52;
-	static unsigned const exponent_bits = 11;
-	static signed const exponent_bias = 1023;
-	static bool const implied_one = true;
-	typedef uint64_t record_type;
+    static unsigned const mantissa_bits = 52;
+    static unsigned const exponent_bits = 11;
+    static signed const exponent_bias = 1023;
+    static bool const implied_one = true;
+    typedef uint64_t record_type;
 };
 
 template <>
 struct float_traits<float>
 {
-	static unsigned const mantissa_bits = 23;
-	static unsigned const exponent_bits = 8;
-	static signed const exponent_bias = 127;
-	static bool const implied_one = true;
-	typedef uint32_t record_type;
+    static unsigned const mantissa_bits = 23;
+    static unsigned const exponent_bits = 8;
+    static signed const exponent_bias = 127;
+    static bool const implied_one = true;
+    typedef uint32_t record_type;
 };
 
 template <typename Float>
 union FloatRec
 {
-	Float val;
-	typename float_traits<Float>::record_type rec;
-	FloatRec(Float const value): val(value) { }
+    Float val;
+    typename float_traits<Float>::record_type rec;
+    FloatRec(Float const value): val(value) { }
 };
 BOOST_STATIC_ASSERT(sizeof(FloatRec<long double>) == sizeof(long double));
 BOOST_STATIC_ASSERT(sizeof(FloatRec<double>) == sizeof(double));
@@ -81,14 +81,14 @@ template <typename Float>
 struct FloatInfo
 {
 private:
-	FloatRec<Float> helper;
+    FloatRec<Float> helper;
 public:
-	bool negative;
-	uint16_t exponent;
-	uint64_t mantissa;
-	float_type number_type;
+    bool negative;
+    uint16_t exponent;
+    uint64_t mantissa;
+    float_type number_type;
 
-	FloatInfo(Float const value);
+    FloatInfo(Float const value);
 };
 
 std::string
--- test-analyze-float.cpp
+++ test-analyze-float.cpp
@@ -9,188 +9,188 @@
 
 BOOST_AUTO_TEST_CASE(extended_zero)
 {
-	FloatInfo<long double> const info(0.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 0);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, zero);
+    FloatInfo<long double> const info(0.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 0);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, zero);
 }
 
 BOOST_AUTO_TEST_CASE(double_zero)
 {
-	FloatInfo<double> const info(0.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 0);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, zero);
+    FloatInfo<double> const info(0.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 0);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, zero);
 }
 
 BOOST_AUTO_TEST_CASE(single_zero)
 {
-	FloatInfo<float> const info(0.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 0);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, zero);
+    FloatInfo<float> const info(0.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 0);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, zero);
 }
 
 BOOST_AUTO_TEST_CASE(extended_one)
 {
-	FloatInfo<long double> const info(1.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 16383);
-	BOOST_CHECK_EQUAL(info.mantissa, 0x8000000000000000ull);
-	BOOST_CHECK_EQUAL(info.number_type, normal);
+    FloatInfo<long double> const info(1.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 16383);
+    BOOST_CHECK_EQUAL(info.mantissa, 0x8000000000000000ull);
+    BOOST_CHECK_EQUAL(info.number_type, normal);
 }
 
 BOOST_AUTO_TEST_CASE(double_one)
 {
-	FloatInfo<double> const info(1.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 1023);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, normal);
+    FloatInfo<double> const info(1.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 1023);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, normal);
 }
 
 BOOST_AUTO_TEST_CASE(single_one)
 {
-	FloatInfo<float> const info(1.0);
-	BOOST_CHECK(!info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 127);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, normal);
+    FloatInfo<float> const info(1.0);
+    BOOST_CHECK(!info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 127);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, normal);
 }
 
 BOOST_AUTO_TEST_CASE(negative_extended_one)
 {
-	FloatInfo<long double> const info(-1);
-	BOOST_CHECK(info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 16383);
-	BOOST_CHECK_EQUAL(info.mantissa, 0x8000000000000000ull);
-	BOOST_CHECK_EQUAL(info.number_type, normal);
+    FloatInfo<long double> const info(-1);
+    BOOST_CHECK(info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 16383);
+    BOOST_CHECK_EQUAL(info.mantissa, 0x8000000000000000ull);
+    BOOST_CHECK_EQUAL(info.number_type, normal);
 }
 
 BOOST_AUTO_TEST_CASE(negative_two_double)
 {
-	FloatInfo<double> const info(-2.0);
-	BOOST_CHECK(info.negative);
-	BOOST_CHECK_EQUAL(info.exponent, 1024);
-	BOOST_CHECK_EQUAL(info.mantissa, 0u);
-	BOOST_CHECK_EQUAL(info.number_type, normal);
+    FloatInfo<double> const info(-2.0);
+    BOOST_CHECK(info.negative);
+    BOOST_CHECK_EQUAL(info.exponent, 1024);
+    BOOST_CHECK_EQUAL(info.mantissa, 0u);
+    BOOST_CHECK_EQUAL(info.number_type, normal);
 }
 
 BOOST_AUTO_TEST_CASE(test_find_last)
 {
-	bitset empty_bitset;
-	BOOST_CHECK_EQUAL(find_last(empty_bitset), bitset::size_type(-1));
+    bitset empty_bitset;
+    BOOST_CHECK_EQUAL(find_last(empty_bitset), bitset::size_type(-1));
 
-	bitset one_bit_set(std::string("100"));
-	BOOST_CHECK_EQUAL(find_last(one_bit_set), 2u);
+    bitset one_bit_set(std::string("100"));
+    BOOST_CHECK_EQUAL(find_last(one_bit_set), 2u);
 
-	bitset many_bits_set(std::string("1111111111111111111111111111111111"));
-	BOOST_CHECK_EQUAL(find_last(many_bits_set), 33u);
+    bitset many_bits_set(std::string("1111111111111111111111111111111111"));
+    BOOST_CHECK_EQUAL(find_last(many_bits_set), 33u);
 }
 
 BOOST_AUTO_TEST_CASE(test_build_bitset)
 {
-	BOOST_CHECK_EQUAL(bitset(), build_bitset(0, 0));
+    BOOST_CHECK_EQUAL(bitset(), build_bitset(0, 0));
 
-	BOOST_CHECK_EQUAL(bitset(std::string("1010101010101010101010101010101010101010101010101010101010101010")), build_bitset(0xAAAAAAAAAAAAAAAAull, 64));
+    BOOST_CHECK_EQUAL(bitset(std::string("1010101010101010101010101010101010101010101010101010101010101010")), build_bitset(0xAAAAAAAAAAAAAAAAull, 64));
 }
 
 BOOST_AUTO_TEST_CASE(test_find_last_int)
 {
-	BOOST_CHECK_EQUAL(size_t(-1), find_last(0));
-	BOOST_CHECK_EQUAL(size_t(0), find_last(1));
-	BOOST_CHECK_EQUAL(size_t(4), find_last(20));
+    BOOST_CHECK_EQUAL(size_t(-1), find_last(0));
+    BOOST_CHECK_EQUAL(size_t(0), find_last(1));
+    BOOST_CHECK_EQUAL(size_t(4), find_last(20));
 }
 
 BOOST_AUTO_TEST_CASE(test_build_bitset_int)
 {
-	BOOST_CHECK_EQUAL(bitset(), build_bitset(0));
-	BOOST_CHECK_EQUAL(bitset(std::string("1")), build_bitset(1));
-	BOOST_CHECK_EQUAL(bitset(std::string("10100")), build_bitset(20));
+    BOOST_CHECK_EQUAL(bitset(), build_bitset(0));
+    BOOST_CHECK_EQUAL(bitset(std::string("1")), build_bitset(1));
+    BOOST_CHECK_EQUAL(bitset(std::string("10100")), build_bitset(20));
 }
 
 BOOST_AUTO_TEST_CASE(test_one_bit_add)
 {
-	BOOST_CHECK_EQUAL(one_bit_add(false, false, false), boost::make_tuple(false, false));
-	BOOST_CHECK_EQUAL(one_bit_add(false, false, true), boost::make_tuple(true, false));
-	BOOST_CHECK_EQUAL(one_bit_add(false, true, false), boost::make_tuple(true, false));
-	BOOST_CHECK_EQUAL(one_bit_add(false, true, true), boost::make_tuple(false, true));
-	BOOST_CHECK_EQUAL(one_bit_add(true, false, false), boost::make_tuple(true, false));
-	BOOST_CHECK_EQUAL(one_bit_add(true, false, true), boost::make_tuple(false, true));
-	BOOST_CHECK_EQUAL(one_bit_add(true, true, false), boost::make_tuple(false, true));
-	BOOST_CHECK_EQUAL(one_bit_add(true, true, true), boost::make_tuple(true, true));
+    BOOST_CHECK_EQUAL(one_bit_add(false, false, false), boost::make_tuple(false, false));
+    BOOST_CHECK_EQUAL(one_bit_add(false, false, true), boost::make_tuple(true, false));
+    BOOST_CHECK_EQUAL(one_bit_add(false, true, false), boost::make_tuple(true, false));
+    BOOST_CHECK_EQUAL(one_bit_add(false, true, true), boost::make_tuple(false, true));
+    BOOST_CHECK_EQUAL(one_bit_add(true, false, false), boost::make_tuple(true, false));
+    BOOST_CHECK_EQUAL(one_bit_add(true, false, true), boost::make_tuple(false, true));
+    BOOST_CHECK_EQUAL(one_bit_add(true, true, false), boost::make_tuple(false, true));
+    BOOST_CHECK_EQUAL(one_bit_add(true, true, true), boost::make_tuple(true, true));
 }
 
 BOOST_AUTO_TEST_CASE(test_ont_bit_subtract)
 {
-	BOOST_CHECK_EQUAL(one_bit_subtract(false, false, false), boost::make_tuple(false, false));
-	BOOST_CHECK_EQUAL(one_bit_subtract(false, false, true), boost::make_tuple(true, true));
-	BOOST_CHECK_EQUAL(one_bit_subtract(false, true, false), boost::make_tuple(true, true));
-	BOOST_CHECK_EQUAL(one_bit_subtract(false, true, true), boost::make_tuple(false, true));
-	BOOST_CHECK_EQUAL(one_bit_subtract(true, false, false), boost::make_tuple(true, false));
-	BOOST_CHECK_EQUAL(one_bit_subtract(true, false, true), boost::make_tuple(false, false));
-	BOOST_CHECK_EQUAL(one_bit_subtract(true, true, false), boost::make_tuple(false, false));
-	BOOST_CHECK_EQUAL(one_bit_subtract(true, true, true), boost::make_tuple(true, true));
+    BOOST_CHECK_EQUAL(one_bit_subtract(false, false, false), boost::make_tuple(false, false));
+    BOOST_CHECK_EQUAL(one_bit_subtract(false, false, true), boost::make_tuple(true, true));
+    BOOST_CHECK_EQUAL(one_bit_subtract(false, true, false), boost::make_tuple(true, true));
+    BOOST_CHECK_EQUAL(one_bit_subtract(false, true, true), boost::make_tuple(false, true));
+    BOOST_CHECK_EQUAL(one_bit_subtract(true, false, false), boost::make_tuple(true, false));
+    BOOST_CHECK_EQUAL(one_bit_subtract(true, false, true), boost::make_tuple(false, false));
+    BOOST_CHECK_EQUAL(one_bit_subtract(true, true, false), boost::make_tuple(false, false));
+    BOOST_CHECK_EQUAL(one_bit_subtract(true, true, true), boost::make_tuple(true, true));
 }
 
 BOOST_AUTO_TEST_CASE(test_multiply)
 {
-	BOOST_CHECK_EQUAL(Multiply(build_bitset(10), build_bitset(50)), build_bitset(500));
-	BOOST_CHECK_EQUAL(Multiply(build_bitset(50), build_bitset(2)), build_bitset(100));
-	BOOST_CHECK_EQUAL(Multiply(build_bitset(0), build_bitset(1)), bitset());
-	BOOST_CHECK_EQUAL(Multiply(build_bitset(0xffffffff), build_bitset(0xffffffff)), build_bitset(18446744065119617025u));
-	BOOST_CHECK_EQUAL(Multiply(bitset(), bitset()), bitset());
+    BOOST_CHECK_EQUAL(Multiply(build_bitset(10), build_bitset(50)), build_bitset(500));
+    BOOST_CHECK_EQUAL(Multiply(build_bitset(50), build_bitset(2)), build_bitset(100));
+    BOOST_CHECK_EQUAL(Multiply(build_bitset(0), build_bitset(1)), bitset());
+    BOOST_CHECK_EQUAL(Multiply(build_bitset(0xffffffff), build_bitset(0xffffffff)), build_bitset(18446744065119617025u));
+    BOOST_CHECK_EQUAL(Multiply(bitset(), bitset()), bitset());
 }
 
 BOOST_AUTO_TEST_CASE(test_subtract)
 {
-	BOOST_CHECK_EQUAL(Subtract(bitset(), bitset()), bitset());
-	BOOST_CHECK_EQUAL(Subtract(build_bitset(500), build_bitset(3)), build_bitset(497));
-	BOOST_CHECK_EQUAL(Subtract(build_bitset(15), build_bitset(15)), bitset());
+    BOOST_CHECK_EQUAL(Subtract(bitset(), bitset()), bitset());
+    BOOST_CHECK_EQUAL(Subtract(build_bitset(500), build_bitset(3)), build_bitset(497));
+    BOOST_CHECK_EQUAL(Subtract(build_bitset(15), build_bitset(15)), bitset());
 }
 
 BOOST_AUTO_TEST_CASE(test_bitset_ge)
 {
-	BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(2))(build_bitset(1)));
-	BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(10))(build_bitset(9)));
-	BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(20))(build_bitset(2)));
-	BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (bitset(std::string("00100")))(bitset(std::string("100"))));
-	BOOST_CHECK_PREDICATE(std::not2(std::greater_equal<bitset>()), (bitset())(build_bitset(2)));
+    BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(2))(build_bitset(1)));
+    BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(10))(build_bitset(9)));
+    BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (build_bitset(20))(build_bitset(2)));
+    BOOST_CHECK_PREDICATE(std::greater_equal<bitset>(), (bitset(std::string("00100")))(bitset(std::string("100"))));
+    BOOST_CHECK_PREDICATE(std::not2(std::greater_equal<bitset>()), (bitset())(build_bitset(2)));
 }
 
 BOOST_AUTO_TEST_CASE(test_divide)
 {
-	BOOST_CHECK_EQUAL(DivideAndRemainder(build_bitset(42), build_bitset(7)), boost::make_tuple(build_bitset(6), 0u));
+    BOOST_CHECK_EQUAL(DivideAndRemainder(build_bitset(42), build_bitset(7)), boost::make_tuple(build_bitset(6), 0u));
 
-	BOOST_CHECK_EQUAL(DivideAndRemainder(bitset(), build_bitset(45)), boost::make_tuple(bitset(), 0u));
+    BOOST_CHECK_EQUAL(DivideAndRemainder(bitset(), build_bitset(45)), boost::make_tuple(bitset(), 0u));
 
-	BOOST_CHECK_EQUAL(DivideAndRemainder(build_bitset(10235), build_bitset(10)), boost::make_tuple(build_bitset(1023), 5u));
+    BOOST_CHECK_EQUAL(DivideAndRemainder(build_bitset(10235), build_bitset(10)), boost::make_tuple(build_bitset(1023), 5u));
 }
 
 BOOST_AUTO_TEST_CASE(test_minimize_mantissa)
 {
-	BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("011000")), 3), boost::make_tuple(bitset(std::string("011000")), 3));
+    BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("011000")), 3), boost::make_tuple(bitset(std::string("011000")), 3));
 
-	BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("0100")), -3), boost::make_tuple(bitset(std::string("0001")), -1));
+    BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("0100")), -3), boost::make_tuple(bitset(std::string("0001")), -1));
 
-	BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("110000")), -3), boost::make_tuple(bitset(std::string("000110")), 0));
+    BOOST_CHECK_EQUAL(minimize_mantissa(bitset(std::string("110000")), -3), boost::make_tuple(bitset(std::string("000110")), 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_remove_fraction)
 {
-	BOOST_CHECK_EQUAL(remove_fraction(build_bitset(2), -4), boost::make_tuple(build_bitset((1*5*5*5*5) * 2), 0, -4));
+    BOOST_CHECK_EQUAL(remove_fraction(build_bitset(2), -4), boost::make_tuple(build_bitset((1*5*5*5*5) * 2), 0, -4));
 
-	BOOST_CHECK_EQUAL(remove_fraction(build_bitset(3), 0), boost::make_tuple(build_bitset(3), 0, 0));
+    BOOST_CHECK_EQUAL(remove_fraction(build_bitset(3), 0), boost::make_tuple(build_bitset(3), 0, 0));
 
-	BOOST_CHECK_EQUAL(remove_fraction(build_bitset(4), 6), boost::make_tuple(build_bitset(4), 6, 0));
+    BOOST_CHECK_EQUAL(remove_fraction(build_bitset(4), 6), boost::make_tuple(build_bitset(4), 6, 0));
 }
 
 BOOST_AUTO_TEST_CASE(test_reduce_binary_exponent)
 {
-	BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("1100")), 4), bitset(std::string("11000000")));
-	BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("1100")), 1), bitset(std::string("11000")));
-	BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("110")), 0), bitset(std::string("110")));
+    BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("1100")), 4), bitset(std::string("11000000")));
+    BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("1100")), 1), bitset(std::string("11000")));
+    BOOST_CHECK_EQUAL(reduce_binary_exponent(bitset(std::string("110")), 0), bitset(std::string("110")));
 }
